fe082df002666d621bca865837b03f8dcf637fbc
Muhammad Haris <114726020+MuhammadHarisBaloch@users.noreply.github.com>
2025-08-03 00:36:45 +0500

    Updating Navbar

--- src/components/BasicUI/index.tsx
+++ src/components/BasicUI/index.tsx
@@ -58,66 +58,24 @@ export default function BasicUI({ children }: PropsWithChildren) {
         </Group>
       </AppShell.Header>
       {/* Manual Mobile Navbar */}
-      {opened && (
-        <div
-          style={{
-            position: "fixed",
-            top: 80,
-            left: 0,
-            width: "100%",
-            backgroundColor: "#000",
-            padding: "1.5rem",
-            zIndex: 9999,
-          }}
-        >
-          <Stack>
-            {Data.HeaderSection.HeaderItems.map((data, index) => (
-              <Link
-                key={index}
-                to={data}
-                spy={true}
-                smooth={true}
-                offset={-80}
-                duration={500}
-                activeClass="active-link"
-                onClick={toggle}
-              >
-                <span className="nav-link">{data}</span>
-              </Link>
-            ))}
-          </Stack>
-        </div>
-      )}
-
-      {/* <AppShell.Navbar
-        bg="black"
-        px="xl"
-        py="lg"
-        hiddenFrom="sm"
-        style={{ height: "100vh", zIndex: 9999 }}
-      >
+      <div className={`mobile-navbar ${opened ? "open" : ""}`}>
         <Stack>
-          {Data.HeaderSection.HeaderItems.map((data, index) => {
-            return (
-              <Link
-                key={index}
-                to={data}
-                spy={true}
-                smooth={true}
-                offset={-80}
-                duration={500}
-                className="hover-cyan-color"
-                activeClass="cyan-color"
-                onClick={() => {
-                  if (opened) toggle();
-                }}
-              >
-                {data}
-              </Link>
-            );
-          })}
+          {Data.HeaderSection.HeaderItems.map((data, index) => (
+            <Link
+              key={index}
+              to={data}
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}
+              activeClass="active-link"
+              onClick={toggle}
+            >
+              <span className="nav-link">{data}</span>
+            </Link>
+          ))}
         </Stack>
-      </AppShell.Navbar> */}
+      </div>
       <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
   );
